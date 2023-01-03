e2850bf5dd8d883b5949e1bab749684a430d28be
shellcodesniper <shellcodesniper@icloud.com>
2023-01-03 22:25:08 +0900

    feat: Out-Bound Function & Global Constants

--- src/func.rs
+++ src/func.rs
@@ -1,2 +1,21 @@
 use super::global::{ GLOBAL_DEGREE };
 // NOTE : Hackable Degree
+
+/// ## Functions for B-Tree
+/// 
+
+/// ### `init()`
+/// 
+/// Initialize the B-Tree
+///
+/// * `degree` : The degree of the B-Tree ( usize )
+/// * `return` : None
+///
+/// # Example
+/// ```
+/// btree::init(3);
+/// ```
+pub fn init(degree: usize) {
+  GLOBAL_DEGREE.store(degree, std::sync::atomic::Ordering::SeqCst);
+}
+
--- src/global.rs
+++ src/global.rs
@@ -1,6 +1,13 @@
-use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::{AtomicUsize, AtomicBool};
 
-pub static GLOBAL_DEGREE: AtomicUsize = AtomicUsize::new(0);
+/// ## Global Variable
+/// 
+/// `GLOBAL_DEGREE` : The degree of the B-Tree
+/// TYPE : GLOBAL DEGREE OF B-Tree, initialize : using `init()` function in `func.rs`
+/// TYPE : B-Tree 의 차수, 전역 변수 <기본: 3> 초기화는 `func.rs` 의 `init()` 에서 진행됨.
+///
+pub static GLOBAL_DEGREE: AtomicUsize = AtomicUsize::new(3);
+pub static GLOBAL_DEGREE_INITIALIZED: AtomicBool = AtomicBool::new(false);
 
 #[cfg(test)]
 mod tests {
@@ -9,7 +16,7 @@ mod tests {
 
   #[tokio::test]
   async fn test_global_degree_load_first() {
-    assert!(GLOBAL_DEGREE.load(Ordering::Relaxed) == 0);
+    assert!(GLOBAL_DEGREE.load(Ordering::Relaxed) == 3);
   }
 
   #[tokio::test]
@@ -17,6 +24,14 @@ mod tests {
     tokio::time::sleep_until(tokio::time::Instant::now() + std::time::Duration::from_secs(1)).await;
     // HACK : This is a hack to make sure the test is run after the previous test
     GLOBAL_DEGREE.store(1, Ordering::SeqCst);
+    GLOBAL_DEGREE_INITIALIZED.store(true, Ordering::SeqCst);
     assert!(GLOBAL_DEGREE.load(Ordering::Relaxed) == 1);
+  }
+
+  #[tokio::test]
+  async fn test_global_degree_is_set() {
+    tokio::time::sleep_until(tokio::time::Instant::now() + std::time::Duration::from_secs(2)).await;
+    // HACK : This is a hack to make sure the test is run after the previous test (degree_store)
+    assert!(GLOBAL_DEGREE_INITIALIZED.load(Ordering::Relaxed));
   }
 }
