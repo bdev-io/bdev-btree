0a4566121f04cdbfc159b76eeb9dd3945309fd87
shellcodesniper <shellcodesniper@icloud.com>
2023-01-06 14:46:25 +0900

    CHORE: UPDATE PRE-PUSH script

--- githooks/pre-push.sh
+++ githooks/pre-push.sh
@@ -4,9 +4,9 @@
 # Coded by: KuuwangE < root@ql.gl > 
 #
 
-PROJECT_DIR=$(dirname `dirname $(realpath $0)`)
+PROJECT_DIR=$(dirname `dirname $(`dirname $(realpath $0)`)`)
 # HACK : This is Hacky Way to Get Project ABSOLUTE PATH Based On CURRENT FILE
 
 # $PROJECT_DIR/bin/test_script.sh
 
-../bin/test_script.sh # This is the way to run script from another directory
+$PROJECT_DIR/bin/test_script.sh # This is the way to run script from another directory
