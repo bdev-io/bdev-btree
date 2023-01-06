8c4a8c5c6ea321011cee4d44f976b969d8b79649
shellcodesniper <shellcodesniper@icloud.com>
2023-01-06 14:44:25 +0900

    CHORE: GIT HOOKS for Test

--- githooks/pre-push.sh
+++ githooks/pre-push.sh
@@ -4,10 +4,9 @@
 # Coded by: KuuwangE < root@ql.gl > 
 #
 
-CURRENT_DIR=$(dirname $(realpath $0))
 PROJECT_DIR=$(dirname `dirname $(realpath $0)`)
 # HACK : This is Hacky Way to Get Project ABSOLUTE PATH Based On CURRENT FILE
 
+# $PROJECT_DIR/bin/test_script.sh
 
-mv $PROJECT_DIR/.git/hooks/pre-push $PROJECT_DIR/.git/hooks/pre-push.bak
-ln -s $CURRENT_DIR/pre-push.sh $PROJECT_DIR/.git/hooks/pre-push
+../bin/test_script.sh # This is the way to run script from another directory
